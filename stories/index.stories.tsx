4690c4bfc9575852316b75d17b8e3c41792d3bd8
leo <leo-ryu@outlook.com>
2019-03-26 22:32:03 +0800

    add App bar

--- stories/index.stories.tsx
+++ stories/index.stories.tsx
@@ -3,17 +3,22 @@ import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
 import { linkTo } from '@storybook/addon-links';
+import AppBar from '../src/compoments/AppBar';
+// import Button from '@material-ui/core/Button'
 
-import { Button, Welcome } from '@storybook/react/demo';
+storiesOf('AppBar', module).add('no prop', () => <AppBar />);
+// storiesOf('Button', module).add('with text', () => <Button variant='outlined'> Hello </Button>);
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+// import { Button, Welcome } from '@storybook/react/demo';
 
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+// storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
+
+// storiesOf('Button', module)
+//   .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
+//   .add('with some emoji', () => (
+//     <Button onClick={action('clicked')}>
+//       <span role="img" aria-label="so cool">
+//         😀 😎 👍 💯
+//       </span>
+//     </Button>
+//   ));
